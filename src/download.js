ee9fd801df657c254fa2b3928c022d08bcdf834d
ushironoko <apple19940820@yahoo.co.jp>
2019-01-05 20:48:53 +0900

    typo fix,

--- src/download.js
+++ src/download.js
@@ -1,7 +1,6 @@
 const request = require('request')
 const fs = require('fs')
 const repo = require('../response.json')
-const imagepath = './images/'
 
 async function imageRequest(url, writeFileStream, filename) {
   await request(url)
@@ -11,18 +10,26 @@ async function imageRequest(url, writeFileStream, filename) {
     })
 }
 
-const parse = repo.map(x => {
-  return x._source.entities
-})
+module.exports = function download() {
+  try {
+    const parse = repo.map(x => {
+      return x._source.entities
+    })
 
-const urls = parse.filter(x => x != null).map(x => {
-  return x.media[0].media_url_https
-})
+    const urls = parse.filter(x => x != null).map(x => {
+      return x.media[0].media_url_https
+    })
 
-urls.map(x => {
-  let s = x.lastIndexOf('/')
-  let filename = x.substr(s + 1)
+    urls.map(x => {
+      let s = x.lastIndexOf('/')
+      let filename = x.substr(s + 1)
 
-  const writeFileStream = fs.createWriteStream(imagepath + filename)
-  imageRequest(x, writeFileStream, filename)
-})
+      let writeFileStream = fs.createWriteStream(
+        process.env.IMAGES_PATH + filename
+      )
+      imageRequest(x, writeFileStream, filename)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,11 +1,24 @@
 require('dotenv').config()
 const jsonWrite = require('./jsonwrite')
 const getPlayResultJSON = require('./elastic_request')
+const download = require('./download')
+const ocr = require('./ocr')
 
 function main() {
-  getPlayResultJSON().then(res => {
-    jsonWrite(res)
-  })
+  //ElasticsearchからJSON取得
+  getPlayResultJSON()
+    .then(res => {
+      //取得したJSONをファイルにエクスポート
+      jsonWrite(res)
+    })
+    .then(() => {
+      //response.json の url を使って画像を取得
+      download()
+    })
+    .then(() => {
+      //images/ 配下の画像を OCR 読み取りして結果をエクスポート
+      ocr()
+    })
 }
 
 main()
